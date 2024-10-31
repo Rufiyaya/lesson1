50a8af9f611c5f455b7e8d4fce577a2acb965c55
Rufiyaya <rufiyayarmakova@gmail.com>
2024-10-31 18:16:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#1st program
+print((9**0.5)*5)
+#2st program
+print(9.99>9.98 and 1000!=1000.1)
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#3st program
+stroka="123.456"
+print(float("123.456")*10)
+print(int(1234.56)%10//1)
+
